4e328cba35a952c79d24c72c268588e0ba136edb
mth-titumir-si <titumir@standard-insights.com>
2024-08-01 14:11:38 +0600

    store connected with base api

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import applyReducer from "./features/apply/applySlice";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
     reducer: {
+        [baseApi.reducerPath]: baseApi.reducer,
         apply: applyReducer,
-    }
+    },
+    middleware: (getDefaultMiddlewares) =>
+        getDefaultMiddlewares().concat(baseApi.middleware),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
